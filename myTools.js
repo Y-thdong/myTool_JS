8f93b35d844c54eee13fa3d4396f47da47109640
Y-thdong <tianhuidong2017@outlook.com>
2020-12-17 23:21:58 +0800

    update

--- myTools.js
+++ myTools.js
@@ -571,3 +571,20 @@ function deepClone (origin, target1) {
     }
     return target2;
 }
+
+
+
+// P288
+function matchesSelector (element, selector) {
+    if (element.matchesSelector) {
+        return element.matchesSelector(selector);
+    } else if (element.msMatchesSelector) {
+        return element.msMatchesSelector(selector);
+    } else if (element.mozMatchesSelector) {
+        return element.mozMatchesSelector(selector);
+    } else if (element.webkitMatchesSelector) {
+        return element.webkitMatchesSelector(selector);
+    } else {
+        console.info("Not supported.");
+    }
+}
